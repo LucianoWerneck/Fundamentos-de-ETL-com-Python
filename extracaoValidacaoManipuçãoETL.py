3e17a463c7294fd715156bb5375d51dc3ae437b0
LucianoWerneck <lulu_werneck@gmail.com.br>
2021-08-24 15:31:31 -0300

    Extração e Validadação de Dados

--- extracaoValidacaoManipuçãoETL.py
+++ extracaoValidacaoManipuçãoETL.py
@@ -1,15 +1,31 @@
-'''Extração e Validando de Dados e aprendendo dataframe'''
+#Extração e Validando de Dados e aprendendo dataframe
 import pandas as pd
 import pandera as pa
 
+#comando para extração organização dos dados
 df = pd.read_csv("ocorrencia_2010_2020.csv", sep=";", parse_dates=["ocorrencia_dia"], dayfirst=True)
+#comando para deletar colunas
 del df["codigo_ocorrencia1"]
 df = df.drop(df.columns[[ 2, 3, 5, 6, 9, 13, 14, 15, 16, 17, 19, 20]], axis=1)
+
+#comando para realizar exibição 10 colunas do dataframe
 pd.set_option('display.max_columns', 10)
+
+#comando para realizar exibição 10 linhas
 print(df.tail(10))
+
+
+#comando lista as informações do arquivo
 print(df.info)
+
+#comando que mostra os tipos de dados das colunas
 print(df.dtypes)
-#print(df.ocorrencia_dia.dt.month)
+
+#comando para acessar a coluna, expecificando a busca por mês após ter
+#sido converter o tipo de obejeto para datetime
+print(df.ocorrencia_dia.dt.month)
+
+#comando de validação de cada dado recebido pelo dataframe
 schema = pa.DataFrameSchema(
     columns = {
         "codigo_ocorrencia":pa.Column(pa.Int, required=False),
